55d7a7f9373f8915b1426a2adcbc782efed89166
nuriza564 <kaharkyzynuriza@gmail.com>
2024-08-05 18:21:53 +0600

    Add files via upload

---
+++ 2hm3/main.js
@@ -0,0 +1,25 @@
+// function square(number) {
+//     return number * number;
+//   }
+  
+//   const result = square(5);
+//   console.log(result); 
+  
+
+//   const double = number => number * 2;
+
+//   const resulte = double(5);
+//   console.log(result); 
+  
+
+
+// доп дз
+const book = {
+    title: "Мастер и Маргарита",
+    author: "Михаил Булгаков",
+    year: 1967
+  };
+  
+  console.log("Название:", book.title); 
+  console.log("Автор:", book.author);   
+  console.log("Год выпуска:", book.year);
